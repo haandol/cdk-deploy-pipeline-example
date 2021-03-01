61af229bef7a4897fcdbd3f244c7ba32a96a6fe2
Dongkyun Lee <dongkyl@amazon.com>
2021-03-01 23:47:09 +0900

    add gs1

--- infra/lib/stacks/storage-stack.ts
+++ infra/lib/stacks/storage-stack.ts
@@ -6,7 +6,7 @@ export class StorageStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    new dynamodb.Table(this, `MyTable`, {
+    const table = new dynamodb.Table(this, `MyTable`, {
       tableName: `${App.Context.ns}MyTable`,
       partitionKey: {
         name: 'PK',
@@ -17,6 +17,17 @@ export class StorageStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    })
+    table.addGlobalSecondaryIndex({
+      indexName: 'GS1',
+      partitionKey: {
+        name: 'GS1PK',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'GS1SK',
+        type: dynamodb.AttributeType.STRING,
+      },
     })
   }
 }
