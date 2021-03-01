1a6cc135e4decb2de2c062db32c86043ff3b6a57
Dongkyun Lee <dongkyl@amazon.com>
2021-03-01 23:41:27 +0900

    rename table

--- infra/lib/stacks/storage-stack.ts
+++ infra/lib/stacks/storage-stack.ts
@@ -1,12 +1,13 @@
-import * as cdk from '@aws-cdk/core';
-import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as cdk from '@aws-cdk/core'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import { App } from '../interfaces/config'
 
 export class StorageStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     new dynamodb.Table(this, `MyTable`, {
-      tableName: 'MyTable',
+      tableName: `${App.Context.ns}MyTable`,
       partitionKey: {
         name: 'PK',
         type: dynamodb.AttributeType.STRING,
